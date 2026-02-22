99a0bbaf22d5e10f0aea9b0fbd84835c2be3ebba
ShadowAdi <shadowshukla76@gmail.com>
2026-02-23 00:22:33 +0530

    added index html

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@
 function App() {
 
   return (
-    <>
+    <main className="flex flex-col items-center ">
       <h1 className="text-2xl font-semibold ">
         Hi This so great
       </h1>
-    </>
+    </main>
   )
 }
 
